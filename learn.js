e3c84a5ebde246597a580c96e07fef3d59755465
exsize <exsize426@mail.ru>
2024-09-29 21:41:17 +0300

    survey

--- learn.js
+++ learn.js
@@ -1,2 +1,10 @@
 const greetings = 'Hello window';
 console.log(greetings)
+
+let answer = prompt('Димон тупой?', '');
+// if(answer == 'Нет' || answer == 'нет'){
+//     alert('Правильно')
+// } else {
+//     alert('Пошел ты Даня')
+// }
+(answer == 'Нет' || answer == 'нет')? alert('Правильно'): alert('Пошел ты Даня');
